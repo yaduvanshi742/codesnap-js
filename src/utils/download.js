1030274efd127cfca8798f51f503b4e5ede607b7
Yadhuvanshi <yaduvanshi742@proton.me>
2026-07-08 14:29:36 +0530

    Add downloadText function for downloading text files

---
+++ src/utils/download.js
@@ -0,0 +1,11 @@
+export function downloadText(filename, content, type = 'text/plain') {
+  const blob = new Blob([content], { type });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = filename;
+  document.body.append(link);
+  link.click();
+  link.remove();
+  URL.revokeObjectURL(url);
+}
